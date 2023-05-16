3dd95036b61390fd053b40c8feb9a8b3324c75d0
Eduardo <114515828+dudurb17@users.noreply.github.com>
2023-05-16 16:08:42 -0300

    aula 16-05
    comando com o metode de banco de dados PDO.

---
+++ pweb1_aula_php-main/BD.class.php
@@ -0,0 +1,49 @@
+<?php
+
+class BD
+{
+    private $host = "localhost";
+    private $dbname = "db_aula";
+    private $port = 3306;
+    private $usuario = "root";
+    private $senha = "";
+    private $db_charset = "utf8";
+
+
+    public function conn()
+    {
+        $conn = "mysql:host=$this->host;dbname=$this->dbname;port=$this->port";
+
+        return new PDO(
+            $conn,
+            $this->usuario,
+            $this->senha,
+            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->db_charset]
+        );
+    }
+
+    public function inserir($dados)
+    {
+        $conn = $this->conn();
+        $sql = "INSERT INTO usuario (nome, email, telefone) VALUES (?, ?, ?);";
+        $st = $conn->prepare($sql);
+        $st->execute([$dados['nome'], $dados['email'], $dados['telefone']]);
+    }
+
+    public function select()
+    {
+        $conn = $this->conn();
+        $sql = "SELECT * FROM usuario;";
+        $st = $conn->prepare($sql);
+        $st->execute();
+
+        return $st->fetchAll(PDO::FETCH_CLASS);
+    }
+    public function deletar($id)
+    {
+        $conn = $this->conn();
+        $sql = "DELETE FROM usuario where id=? ";
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
+    }
+}
