617d39e99233df882cd4fbf27159be49d52d3dc5
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-24 22:33:49 -0300

    button de voltar

--- PWe1_aula_php/Ex_Crud/paginas/formularioList.php
+++ PWe1_aula_php/Ex_Crud/paginas/formularioList.php
@@ -19,25 +19,26 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
-    <div class="container">
-        <h1>Listagem dos dados</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">name</th>
-                    <th scope="col">cpf</th>
-                    <th scope="col">idade</th>
-                    <th scope="col">ações</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <form action="formularioList.php" method="get">
-                    <?php
+    <main>
+        <div class="container">
+            <h1>Listagem dos dados</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">cpf</th>
+                        <th scope="col">idade</th>
+                        <th scope="col">ações</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <form action="formularioList.php" method="get">
+                        <?php
             foreach ($load as $item) {
                 echo "<tr>";
                 echo "<td>".$item->id."</td>";
@@ -48,10 +49,13 @@
                 echo "<tr>";
             }
             ?>
-                </form>
+                    </form>
 
-        </table>
-    </div>
+            </table>
+        </div>
+        <input type="button" value="voltar" onclick="window.location.href = 'formularioFrom.php'">
+    </main>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
